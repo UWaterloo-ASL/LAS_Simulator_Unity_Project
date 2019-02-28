002b8375603e87441796bb74be04b5bf47a2b36d
Lingheng Meng <linghengmeng@yahoo.com>
2019-02-28 14:43:46 -0500

    add Daiwei's VisitorAgent.cs

--- Assets/Scripts/VisitorAgent.cs
+++ Assets/Scripts/VisitorAgent.cs
@@ -1,27 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using MLAgents;
+using UnityEngine.AI;
 
 public class VisitorAgent : Agent
 {
+    public LASAgent LASAgent;
+    public NavMeshAgent NavAgent;
+    // Get Observation
     public override void CollectObservations()
     {
-        AddVectorObs(0);
+        // Observe LED
+        for (int i = 0; i < LASAgent.ledList.Count; i += 1)
+        {
+            //ledList[i].GetComponent<LEDLightIntensity>().SetLedIntensity(vectorAction[i]);
+            float intensity = LASAgent.ledList[i].transform.Find("Point Light").GetComponent<Light>().intensity;
+            AddVectorObs(intensity);
+        }
+        for (int i = 0; i < LASAgent.ledList.Count; i += 1)
+        {
+            //ledList[i].GetComponent<LEDLightIntensity>().SetLedIntensity(vectorAction[i]);
+            float x = LASAgent.ledList[i].transform.Find("Point Light").GetComponent<Light>().transform.position.x;
+            float y = LASAgent.ledList[i].transform.Find("Point Light").GetComponent<Light>().transform.position.y;
+            AddVectorObs(x);
+            AddVectorObs(y);
+        }
     }
 
-    // Take Action and Add Reward
     public override void AgentAction(float[] vectorAction, string textAction)
     {
-
-        Debug.Log("AgentAction");
-
-        AddReward(0);
-        Debug.Log("Calculate Reward");
-    }
-
-    public override void AgentReset()
-    {
-        Debug.Log("AgentReset");
+        if (NavAgent.remainingDistance == 0)
+        {
+            Vector3 destination = Vector3.zero;
+            destination.x = vectorAction[0];
+            destination.z = vectorAction[1];
+            NavAgent.SetDestination(destination);
+        }
     }
 }
